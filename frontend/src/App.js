60cfd5de2dd9ae3046ae71d18ab8b73103b462d1
Gregers Boye-Jacobsen <gregers.boye@gmail.com>
2023-05-05 14:16:38 +0200

    Cleanup and commenting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,18 @@ import './App.css';
 import {useState} from "react";
 
 function App() {
-  const [animate, setAnimate]= useState(false);
-  const [quote, setQuote]= useState({quote: 'Never insult seven men, when all you got is a six-shooter', author: 'Col. Sherman Potter'})
-  async function getData(){
+  const [animate, setAnimate] = useState(false);
+  const [quote, setQuote] = useState({
+    quote: 'Never insult seven men, when all you got is a six-shooter',
+    author: 'Col. Sherman Potter'
+  })
+
+  async function getData() {
     setAnimate(true);
-    setTimeout(() => {setQuote(data); setAnimate(false); }, 1000)
+    setTimeout(() => {
+      setQuote(data);
+      setAnimate(false);
+    }, 1000)
     const quote = await fetch('http://localhost:3001/random');
     const data = await quote.json();
     console.log('getting data');
@@ -17,10 +24,13 @@ function App() {
 
   return (
     <div className="App">
-      <div className={`container ${animate ? 'container_anim' : ''}`} onClick={getData} onAnimationEnd={() => {console.log('done')}}>
-<h1>{quote.quote}</h1>
-      <span style={{color:'#'}}>- {quote.author}</span>
-    </div></div>
+      <div className={`container ${animate ? 'container_anim' : ''}`} onClick={getData} onAnimationEnd={() => {
+        console.log('done')
+      }}>
+        <h1>{quote.quote}</h1>
+        <span style={{color: '#'}}>- {quote.author}</span>
+      </div>
+    </div>
   );
 }
 
--- node/src/index.js
+++ node/src/index.js
@@ -1,58 +1,68 @@
+//Set up express
 const express = require('express');
 
 const app = express();
 const port = 3001;
-const {MongoClient} = require('mongodb');
 const cors = require('cors');
-const ResponseTimer = require('./models/responseTimer');
-const logger = require('./logger');
-// or as an es module:
-// import { MongoClient } from 'mongodb'
 
-// Connection URL/:
-const url = 'mongodb://mongo:27017';
-const client = new MongoClient(url);
-// Database Name
-const dbName = 'quoteDatabase';
+// Allow requests from everywhere
 app.use(cors({
   origin: '*'
 }));
 app.options('*', cors());
-app.get('/', async (req, res) => {
 
+const ResponseTimer = require('./models/responseTimer');
+const logger = require('./logger');
+
+// Connect to the database:
+const {MongoClient} = require('mongodb');
+const url = 'mongodb://mongo:27017';
+const client = new MongoClient(url);
+const dbName = 'quoteDatabase';
+
+/**
+ * Set up routes
+ */
+
+// Get all quotes
+app.get('/', async (req, res) => {
   const quoteList = await getQuotes();
   res.send(quoteList);
   logger.info(quoteList);
 });
+
+// Get a random quote
 app.get('/random', async (req, res) => {
   const quoteList = await getQuotes();
   const quote = quoteList[Math.floor(Math.random() * quoteList.length)];
-
   res.send(quote);
   logger.info(quote);
 });
 
+// Start listening
 app.listen(port, () => {
   logger.info(`Example app listening on port ${port}`);
   logger.info('Paw patrol ready for action Ryder, SIR!');
 });
 
-
+// Get quotes from database
 async function getQuotes() {
   const timer = new ResponseTimer();
-  // Use connect method to connect to the server
+  // Connect to the server
   await client.connect();
   timer.setConnection();
   logger.info(`connected to server after ${timer.connectTime}ms`);
   const db = client.db(dbName);
   const collection = db.collection('documents');
+
+  // Retrieve quotelist
   const quoteList = await collection.find({}).toArray();
   timer.setDocuments();
   logger.info(`Fetched documents after ${timer.documentTime}`);
   logger.info(`Found documents: ${quoteList.length}ms`);
-  // the following code examples can be pasted here...
   timer.setResponse();
 
   logger.info(`Sending response after ${timer.responseTime} ms`);
+
   return quoteList;
 }
